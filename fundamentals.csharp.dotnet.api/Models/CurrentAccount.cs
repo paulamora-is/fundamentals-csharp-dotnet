9153fb837379c95653e39444ff9e0c3fe9f73b87
paulamora-is <pmorais78@gmail.com>
2025-11-05 21:17:41 -0300

    added account validation

--- fundamentals.csharp.dotnet.api/Models/CurrentAccount.cs
+++ fundamentals.csharp.dotnet.api/Models/CurrentAccount.cs
@@ -2,15 +2,48 @@ using fundamentals.csharp.dotnet.api.Enums;
 
 namespace fundamentals.csharp.dotnet.api.Models;
 
-public class CurrentAccount
+public class CurrentAccount(int id, string accountNumber, string agencyNumber,
+                            decimal balance, AccountType accountType, Client client,
+                            AccountStatus accountStatus, DateTime createdAt, DateTime closedAt)
 {
-    public Guid Id { get; set; }
-    public string AccountNumber { get; set; }
-    public string AgencyNumber { get; set; }
-    public decimal Balance { get; set; }
-    public AccountType AccountType { get; set; }
-    public Client Client { get; set; }
-    public AccountStatus AccountStatus { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime ClosedAt { get; set; }
+    public int Id { get; set; } = id;
+    public string AccountNumber { get; set; } = accountNumber;
+    public string AgencyNumber { get; set; } = agencyNumber;
+    public decimal Balance { get; set; } = balance;
+    public AccountType AccountType { get; set; } = accountType;
+    public Client Client { get; set; } = client;
+    public AccountStatus AccountStatus { get; set; } = accountStatus;
+    public DateTime CreatedAt { get; set; } = createdAt;
+    public DateTime ClosedAt { get; set; } = closedAt;
+
+    public bool IsEnoughBalance(decimal amount)
+    {
+        return Balance >= amount;
+    }
+
+    public bool IsActive(CurrentAccount currentAccount)
+    {
+        return currentAccount.AccountStatus.Equals(AccountStatus.Active);
+    }
+
+    public void Deposit(decimal amount, CurrentAccount targetAccount, CurrentAccount sourceAccount)
+    {
+        if (!IsActive(targetAccount))
+        {
+            throw new InvalidOperationException("Cannot deposit to an inactive account.");
+        }
+
+        if(!IsEnoughBalance(amount))
+        {
+            throw new InvalidOperationException("Insufficient balance for the deposit.");
+        }
+
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Deposit amount must be greater than zero.");
+        }
+
+        targetAccount.Balance += amount;
+        sourceAccount.Balance -= amount;
+    }
 }

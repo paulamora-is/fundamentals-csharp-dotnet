3d1d0db7085e2a3145b24ef1b224e96f7fde1b74
paulamora-is <pmorais78@gmail.com>
2025-11-05 21:17:18 -0300

    added constructor

--- fundamentals.csharp.dotnet.api/Models/Client.cs
+++ fundamentals.csharp.dotnet.api/Models/Client.cs
@@ -2,7 +2,19 @@ namespace fundamentals.csharp.dotnet.api.Models;
 
 public record Client
 {
-    public Guid Id { get; init; }
+    public Client(int id, string name, string document, DateTime birthDate, string email,
+                    Address address, Phone phone)
+    {
+        Id = id;
+        Name = name;
+        Document = document;
+        BirthDate = birthDate;
+        Email = email;
+        Address = address;
+        Phone = phone;
+    }
+
+    public int Id { get; init; }
     public string Name { get; init; }
     public string Document { get; init; }
     public DateTime BirthDate { get; init; }

48bf379ef15f68bee335b02a5767259064389864
paulamora-is <pmorais78@gmail.com>
2025-11-05 21:17:00 -0300

    added constructor

--- fundamentals.csharp.dotnet.api/Models/Address.cs
+++ fundamentals.csharp.dotnet.api/Models/Address.cs
@@ -2,6 +2,19 @@ namespace fundamentals.csharp.dotnet.api.Models;
 
 public class Address
 {
+    public Address(string street, string number, string complement, string neighborhood, string city, string state, string zipCode)
+    {
+        Street = street;
+        Number = number;
+        Complement = complement;
+        Neighborhood = neighborhood;
+        City = city;
+        State = state;
+        ZipCode = zipCode;
+    }
+
+    public Address() { }
+
     public string Street { get; set; }
     public string Number { get; set; }
     public string Complement { get; set; }
@@ -9,4 +22,4 @@ public class Address
     public string City { get; set; }
     public string State { get; set; }
     public string ZipCode { get; set; }
- }
+}
--- fundamentals.csharp.dotnet.api/Models/Phone.cs
+++ fundamentals.csharp.dotnet.api/Models/Phone.cs
@@ -2,6 +2,15 @@ namespace fundamentals.csharp.dotnet.api.Models;
 
 public class Phone
 {
+    public Phone(string countryCode, string areaCode, string number)
+    {
+        CountryCode = countryCode;
+        AreaCode = areaCode;
+        Number = number;
+    }
+
+    public Phone() { }
+
     public string CountryCode { get; set; }
     public string AreaCode { get; set; }
     public string Number { get; set; }

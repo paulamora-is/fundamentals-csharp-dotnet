898b686094e88df9e5be95efc0adb4c333a3c952
paulamora-is <pmorais78@gmail.com>
2025-11-06 21:31:46 -0300

    added constructor

--- fundamentals.csharp.dotnet.api/Models/Transaction.cs
+++ fundamentals.csharp.dotnet.api/Models/Transaction.cs
@@ -4,7 +4,17 @@ namespace fundamentals.csharp.dotnet.api.Models;
 
 public record Transaction
 {
-    public Guid Id { get; init; }
+    public Transaction(int id, TransactionType transactionType, CurrentAccount originalCurrentAccount, CurrentAccount targetCurrentAccount, decimal amount, DateTime dateTransaction)
+    {
+        Id = id;
+        TransactionType = transactionType;
+        OriginalCurrentAccount = originalCurrentAccount;
+        TargetCurrentAccount = targetCurrentAccount;
+        Amount = amount;
+        DateTransaction = dateTransaction;
+    }
+
+    public int Id { get; init; }
     public TransactionType TransactionType { get; init; }
     public CurrentAccount OriginalCurrentAccount { get; init; }
     public CurrentAccount TargetCurrentAccount { get; init; }
